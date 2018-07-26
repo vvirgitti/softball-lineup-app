7d757e7dd4b74a4404e864326616d651e04b1cc9
Vanessa Virgitti <virgitti@gmail.com>
2018-07-26 13:08:55 +0100

    Added some elements on the page that are served through firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <header>
+            <div className='wrapper'>
+              <h1>Softball Lineup App</h1>
+              
+            </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='container'>
+          <section className='add-player'>
+              <form>
+                <input type="text" name="name" placeholder="What's your name?" />
+                <button>Add Player</button>
+              </form>
+          </section>
+          <section className='display-player'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
