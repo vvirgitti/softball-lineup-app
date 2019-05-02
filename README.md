d9ae7ab6e1fe26a050983ea038d559f0667bd4b1
Vanessa Virgitti <virgitti@gmail.com>
2019-05-02 18:51:42 +0100

    Adding bootstrap

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Softball Lineup App
 
-Simple app that 
+Simple softball app to create lineups based on stats from Google Sheet.
 
+1/ Create player
+2/ Save player in Google Firebase DB
+3/ Read stats from Google Sheet
+4/ Save stats for each player in Google Firebase DB
+5/ Create lineup
 
 ## Build with
 
 - React
-- Firebase suite: Firebase Hosting, Firebase Realtime DB
+- Firebase suite: Firebase Hosting, Firebase Cloud Firestore DB
+
+
+## How to start it
+`npm start`
+The server will start on `http://localhost:3000`
--- src/App.js
+++ src/App.js
@@ -40,25 +40,65 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+      </head>
       <header>
-        <div className='wrapper'>
+        <div className="wrapper">
           <h1>Softball Lineup App</h1>
         </div>
         </header>
-        <div className='container'>
-          <section className='add-player'>
+        <div className="container">
+          {/* <section className='add-player'>
             <form onSubmit={this.handleSubmit}>
               <input type="text" name="playerName" placeholder="Name" onChange={this.handleNameChange} value={this.state.playerName}/>
               <input type="text" name="playerGender" placeholder="Gender" onChange={this.handleGenderChange} value={this.state.playerGender}/>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="female" value="female" />
+                <label class="form-check-label" for="female">Female</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="male" value="male" />
+                <label class="form-check-label" for="male">Male</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="non-binary" value="non-binary" />
+                <label class="form-check-label" for="non-binary">Non Binary</label>
+              </div>
               <button>Add Player</button>
             </form>
-          </section>
-          <section className='display-player'>
+          </section> */}
+          {/* <section className='display-player'>
             <div className='wrapper'>
               <ul>
               </ul>
+            </div> */}
+          {/* </section> */}
+
+          <div class="card">
+            <div class="card-header">
+              <strong>Player</strong>
             </div>
-          </section>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name" />
+                  <div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+  <label class="form-check-label" for="inlineRadio1">1</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+  <label class="form-check-label" for="inlineRadio2">2</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+  <label class="form-check-label" for="inlineRadio3">3</label>
+</div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     );
